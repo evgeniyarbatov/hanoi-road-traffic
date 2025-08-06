c851159e392e1c251d2493a767cf0d361aede1c4
Evgeny Arbatov <arbatovevgeny@gmail.com>
2025-08-06 16:18:16 +0700

    Let's get the min distance to other ways to filter better

--- scripts/filter.py
+++ scripts/filter.py
@@ -3,6 +3,27 @@ import requests
 
 import pandas as pd
 
+from geopy.distance import geodesic
+
+def min_distance(df):
+    distances = [None]  # First point has no previous point
+
+    for idx in range(1, len(df)):
+        print(idx)
+        current_point = (df.iloc[idx]['lat'], df.iloc[idx]['lon'])
+        min_distance = float('inf')
+
+        for prev_idx in range(idx):
+            prev_point = (df.iloc[prev_idx]['lat'], df.iloc[prev_idx]['lon'])
+            distance = geodesic(current_point, prev_point).meters
+            if distance < min_distance:
+                min_distance = distance
+
+        distances.append(min_distance)
+
+    df['min_distance'] = distances
+    return df
+
 def osrm_format(coords):
     lat, lon = coords
     return f"{lon},{lat}"
@@ -33,6 +54,8 @@ def main(
     start = (float(start_lat), float(start_lon))
     
     df = pd.read_csv(filename)
+    
+    df = min_distance(df)
         
     df['distance'] = df.apply(
         lambda row: get_distance(
